afb4341fb0eda0a9cbe56d1c17ff8e4f6a94bb2c
mandober <ivanilic1975@gmail.com>
2017-07-09 12:48:44 +0200

    reorder

---
+++ 01_primitives/06_null/null.js
@@ -0,0 +1,8 @@
+// null
+
+// means 'no object'
+
+
+// 6.1.2 The Null Type
+
+// The Null type has exactly one value, called null.
--- api/web-workers/worker.js
+++
@@ -1,6 +0,0 @@
-onmessage = function (e) {
-    console.log('[worker] Message received from main script');
-    var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
-    console.log('[worker] Posting message back to main script');
-    postMessage(workerResult);
-}
